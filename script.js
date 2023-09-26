1980918ef57b77b9dd39ecc9c7896bca65f5f2a1
Florin1103 <florinboboc11@gmail.com>
2023-09-26 20:07:00 +0300

    Ternary Operators

--- script.js
+++ script.js
@@ -174,3 +174,17 @@
 //     console.log("There is other color") ;
 // }
 
+// Ternary Operator.
+
+
+let number1 = 10;
+let number2 = 2;
+
+let maxValue;
+if(number1 > number2){
+  maxValue = number1;
+}else{
+  maxValue = number2;
+}
+let max = number1 > number2 ? number1 : number2;
+console.log(max);
