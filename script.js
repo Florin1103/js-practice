02dab636fcbdedf761c4362327f2974d1fd6ce10
Florin1103 <florinboboc11@gmail.com>
2023-10-04 23:15:46 +0300

    map, filter , join, array method

--- script.js
+++ script.js
@@ -881,3 +881,16 @@
 // const docName = doctor.map((doc) => doc.name);
 // console.log(docName);
 
+// ES6 Feature : Filtering an Array
+
+// let numbers = [1,2,3,4,5,6];
+// let onlyOddNumbers = [];
+// for (let num of numbers){
+//   if(num % 2 !== 1){
+//     onlyOddNumbers.push(num);
+//   }
+// }
+// console.log(onlyOddNumbers);
+
+// const onlyOddNumbers = numbers.filter((num) => num % 2 === 1);
+// console.log(onlyOddNumbers);
