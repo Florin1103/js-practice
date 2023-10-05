8d586d173ad0e43f3ee1acb8d69495607a096c91
Florin1103 <florinboboc11@gmail.com>
2023-10-05 14:46:14 +0300

    errors handling

--- script.js
+++ script.js
@@ -1030,3 +1030,23 @@
 
 // trimname = trimname.trimEnd();
 // console.log(trimname);
+
+
+// Different types of errors in JavaScript
+
+// function div(a,b) {
+//   if(b === 0){
+//     throw new SyntaxError("Second Parameter can't be zero");
+//   }
+//   return a / b;
+// }
+
+
+// try {console.log(div(4,0));}
+// catch(e){ console.log(e);
+//           console.log(e.name);
+//           console.log(e.message);
+// }
+// finally {console.log(`It's work perfectly`) }
+
+// console.log("Hi House");
